0a9cd1205a6d05a009844645c2831f41c5b0af8b
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-04 17:43:35 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(typeof "Hello"); // string
+console.log(typeof 29); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol('id')); // symbol
+console.log(typeof BigInt(9875445677654334456666544n)); // BigInt
